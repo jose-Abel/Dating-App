341e58cb1baaa306c3a91ebb786f33d72840fcc3
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-14 21:02:48 -0400

    Extend the User entity to include more properties. Created the photo class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace API.Entities
 {
   public class AppUser
@@ -9,5 +11,27 @@ namespace API.Entities
     public byte[] PasswordHash { get; set; }
 
     public byte[] PasswordSalt { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string KnownAs { get; set; }
+
+    public DateTime Created { get; set; } = DateTime.Now;
+
+    public DateTime LastActive { get; set; } = DateTime.Now;
+
+    public string Gender { get; set; }
+
+    public string Introduction { get; set; }
+
+    public string LookingFor { get; set; }
+
+    public string Interests { get; set; }
+
+    public string City { get; set; }
+
+    public string Country { get; set; }
+
+    public ICollection<Photo> Photos { get; set; }
   }
 }
