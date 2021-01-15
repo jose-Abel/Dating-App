add668716b9cebdb992c16eaee035f268ca1ac8b
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-14 22:08:02 -0400

    Create the Seed class and the UserSeed.json file to populate the database with data. Update the Main class to sent the seed data to the database

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,23 +9,22 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+  [Authorize]
   public class UsersController : BaseApiController
   {
     private readonly DataContext _context;
-    
+
     public UsersController(DataContext context)
     {
       _context = context;
     }
 
     [HttpGet]
-    [AllowAnonymous]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
       return await _context.Users.ToListAsync();
     }
 
-    [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
