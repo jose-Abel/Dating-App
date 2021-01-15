bd448065f3e5a90d5611dd0377c2da290193bd9c
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-14 21:11:31 -0400

    Create GetAge method in the AppUser entity and DateTimeExtensions class to calculate the age of the user

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -11,7 +13,6 @@ namespace API.Entities
     public byte[] PasswordHash { get; set; }
 
     public byte[] PasswordSalt { get; set; }
-
     public DateTime DateOfBirth { get; set; }
 
     public string KnownAs { get; set; }
@@ -33,5 +34,11 @@ namespace API.Entities
     public string Country { get; set; }
 
     public ICollection<Photo> Photos { get; set; }
+
+
+    public int GetAge()
+    {
+      return DateOfBirth.CalculateAge();
+    }
   }
 }
