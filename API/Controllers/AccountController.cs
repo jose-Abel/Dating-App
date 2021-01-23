cbfe39504d0708458cbcea3e1111aa4fa37c142a
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-23 12:24:59 -0400

    Update the photo-editor.component.ts and its html to give the user the option to set the main photo through the setMainPhoto method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -71,7 +71,6 @@ namespace API.Controllers
         Username = user.UserName,
         Token = _tokenService.CreateToken(user),
         PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
-
       };
     }
 
