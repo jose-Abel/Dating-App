e970f9d037fef9263289a10a4cba8a334d5c3729
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-23 16:52:11 -0400

    Set up the registration of a new user from the Angular frontend

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,12 +11,11 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
-
-  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -45,16 +45,12 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.model).subscribe(response => {
-    //   console.log(response);
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
 
-    //   this.cancel();
-
-    // }, error => {
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // });
+    }, error => {
+      this.validationErrors = error;
+    });
   }
 
   cancel() {
