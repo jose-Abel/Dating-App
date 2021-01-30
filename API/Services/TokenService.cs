6143e2c483d31ab71f3de94db5923f5bc8c53f64
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-30 10:27:32 -0400

    Create functionality of loging user activity through the LogUserActivity class

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -11,36 +11,38 @@ using System.IdentityModel.Tokens.Jwt;
 
 namespace API.Services
 {
-    public class TokenService : ITokenService
+  public class TokenService : ITokenService
+  {
+    private readonly SymmetricSecurityKey _key;
+
+    public TokenService(IConfiguration config)
     {
-        private readonly SymmetricSecurityKey _key;
-
-        public TokenService(IConfiguration config)
-        {
-            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
-        }
-
-        public string CreateToken(AppUser user) 
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
-            };
-
-            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
-
-            var tokenDescriptor = new SecurityTokenDescriptor 
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
-                SigningCredentials = creds
-            };
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return tokenHandler.WriteToken(token);
-        }
+      _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
     }
+
+    public string CreateToken(AppUser user)
+    {
+      var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+            };
+
+      var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(7),
+        SigningCredentials = creds
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+
+      return tokenHandler.WriteToken(token);
+    }
+  }
 }
