28c0b31e72beb9628ce5fc6a82b6b9a939b2be94
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-03 17:55:59 -0400

    Create the ILikeRepository, the LikesRepository implementation of it and added to the ApplicationServiceExtension

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -19,6 +19,8 @@ namespace API.Extensions
 
       services.AddScoped<IPhotoService, PhotoService>();
 
+      services.AddScoped<ILikesRepository, LikesRepository>();
+
       services.AddScoped<LogUserActivity>();
 
       services.AddScoped<IUserRepository, UserRepository>();
