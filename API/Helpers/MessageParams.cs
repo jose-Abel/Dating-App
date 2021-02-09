45134bd7bd324d75213a84a5a7abe302e8429f8b
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-08 21:40:37 -0400

    Create the HttpGet route for the GetMessagesForUser method

---
+++ API/Helpers/MessageParams.cs
@@ -0,0 +1,11 @@
+namespace API.Helpers
+{
+  public class MessageParams : PaginationParams
+  {
+    public string Username { get; set; }
+
+    public string Container { get; set; } = "Unread";
+
+
+  }
+}
