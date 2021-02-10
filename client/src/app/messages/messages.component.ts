dfb253b760bd3374f3981b762e9a4d6aa3343d55
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-09 20:50:31 -0400

    Create the messages.component.html to display the inbox, outbox and unread messages in the client

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -11,7 +11,7 @@ import { MessageService } from '../_services/message.service';
 export class MessagesComponent implements OnInit {
   messages: Message[];
   pagination: Pagination;
-  container = 'Inbox';
+  container = 'Outbox';
   pageNumber = 1;
   pageSize = 5;
 
